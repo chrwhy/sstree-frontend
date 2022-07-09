a3578ebc75b5d13fc634a44187921036105524b3
Stephen CHEN <chrwhy@gmail.com>
2022-07-09 15:19:08 +0800

    public access

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
     devServer: {
         disableHostCheck: true,
         port: 8082,
+        host: '0.0.0.0',
         proxy: {
             '^/search': {
                 target: 'http://localhost:8081'
